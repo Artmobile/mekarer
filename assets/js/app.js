88e897dd141ee810598ae81d62da0a5a80497d97
Artmobile <artmobile@gmail.com>
2012-10-30 07:23:14 +0200

    Took the code from Icenium

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,44 +1,60 @@
-// this global variable is where all the script goes so that
-// it doesn't polute the global namespace
-var MYAPP = MYAPP || {};
+// JavaScript Document
 
-MYAPP.run = (function() {
-	// create the Kendo UI Mobile application
-    MYAPP.app = new kendo.mobile.Application(document.body, { transition: "slide" }); 
-});
+// Wait for PhoneGap to load
+document.addEventListener("deviceready", onDeviceReady, false);
 
-// this is the function returns a Kendo UI DataSource
-// which reads the top threads off of the programming.reddit
-// datasource
-MYAPP.reddit = kendo.data.DataSource.create({
-	// set the data to a local array of object
-	transport: {
-		read: "http://reddit.com/r/programming.json"
-	},
-	schema: {
-		data: "data.children",
-		fields: {
-			title: "data.title"
-		}
-	}
-});
+// PhoneGap is ready
+function onDeviceReady() {
+    getLocation();
+}
 
-// this is called when the intial view shows. it prevents the flash
-// of unstyled content (FOUC)
-MYAPP.show = (function() {
-	$(document.body).show();
-});
+function getLocation() {
+    navigator.geolocation.getCurrentPosition(onGeolocationSuccess, onGeolocationError);
+}
 
-// this function runs at startup and attaches to the 'deviceready' event
-// which is fired by PhoneGap when the hardware is ready for native API
-// calls. It is self invoking and will run immediately when this script file is 
-// loaded.
-(function() {
-    if (navigator.userAgent.indexOf('Browzr') > -1) {
-        // blackberry
-        setTimeout(MYAPP.run, 250)    
-    } else {
-        // attach to deviceready event, which is fired when phonegap is all good to go.
-        document.addEventListener('deviceready', MYAPP.run, false);
-    }
-})();
+//=======================Say Hello (Page 1) Operations=======================//
+function sayHello() {
+    var sayHelloInputElem = document.getElementById('helloWorldInput');
+    var sayHelloTextElem = document.getElementById('helloWorldText');
+    var inputText = document.getElementById('txtName');
+
+    sayHelloTextElem.innerHTML = 'Hello, ' + inputText.value + '!';
+    sayHelloTextElem.style.display = 'block';
+    sayHelloInputElem.style.display = 'none';
+}
+
+function sayHelloReset() {
+    var sayHelloInputElem = document.getElementById('helloWorldInput');
+    var sayHelloTextElem = document.getElementById('helloWorldText');
+    var inputText = document.getElementById('txtName');
+
+    inputText.value = '';
+    sayHelloTextElem.style.display = 'none';
+    sayHelloInputElem.style.display = 'block';
+}
+
+//=======================Geolocation Operations=======================//
+// onGeolocationSuccess Geolocation
+function onGeolocationSuccess(position) {
+    // Use Google API to get the location data for the current coordinates
+    var geocoder = new google.maps.Geocoder();
+    var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    geocoder.geocode({ "latLng": latlng }, function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            if ((results.length > 1) && results[1]) {
+                $("#myLocation").html(results[1].formatted_address);
+            }
+        }
+    });
+
+    // Use Google API to get a map of the current location
+    // http://maps.googleapis.com/maps/api/staticmap?size=280x300&maptype=hybrid&zoom=16&markers=size:mid%7Ccolor:red%7C42.375022,-71.273729&sensor=true
+    var googleApis_map_Url = 'http://maps.googleapis.com/maps/api/staticmap?size=300x300&maptype=hybrid&zoom=16&sensor=true&markers=size:mid%7Ccolor:red%7C' + latlng;
+    var mapImg = '<img src="' + googleApis_map_Url + '" />';
+    $("#map_canvas").html(mapImg);
+}
+
+// onGeolocationError Callback receives a PositionError object
+function onGeolocationError(error) {
+    $("#myLocation").html("<span class='err'>" + error.message + "</span>");
+}
