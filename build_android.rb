66b1c88dda6df36e2a6bc9ac5b5724c3f3ea6a89
Artmobile <artmobile@gmail.com>
2012-10-31 13:02:49 +0200

    auto commit as part of script

--- build_android.rb
+++ build_android.rb
@@ -53,10 +53,10 @@ def rebuild()
 	puts "]"
 	puts "Done. Now downloading."
 
-	download=`curl -L -s -u #{$creds} -o #{title}-debug.apk #{FILEPATH}/#{$project}/download/android`
+	download=`curl -L -s -u #{$creds} -o #{title}-debug.apk #{$FILEPATH}/#{$project}/download/android`
 
 	`adb uninstall #{package}`
-	`adb install -r ./#{title}-debug.apk`
+	`adb install -r .//#{title}-debug.apk`
 end
 
 Dir.chdir("#{projectPath}") do 
