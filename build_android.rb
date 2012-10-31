7ad045e475892295a054ee21b4a764ae3e54eee8
Artmobile <artmobile@gmail.com>
2012-10-31 16:14:58 +0200

    auto commit as part of script

--- build_android.rb
+++ build_android.rb
@@ -41,7 +41,7 @@ def rebuild()
 	puts
 	##Request Rebuild
 	print "Requesting Rebuild. "
-	request=`curl -s -u #{$creds} -X PUT -d 'data={"pull":"true"}' #{$APIcall}`
+	request=`curl -u #{$creds} -X PUT -d 'data={"pull":"true"}' #{$APIcall}`
 	donecheck=""
 
 	print "Rebuilding: [-"
