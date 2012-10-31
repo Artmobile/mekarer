713b4b88c139e99d53114de1cd9d0b489bb17b20
Artmobile <artmobile@gmail.com>
2012-10-31 13:01:19 +0200

    auto commit as part of script

--- build_android.rb
+++ build_android.rb
@@ -3,23 +3,23 @@ require 'highline/import'
 require 'json'
 require 'base64'
 
-project="232937"
+bp="c2l4cGVucw==".unpack("m")[0] #get_password("Enter PhoneGap Build password")
 
-username="artmobile@gmail.com"
+$project="232937"
+$username="artmobile@gmail.com"
+$APIPATH="https://build.phonegap.com/api/v1/apps"
+$FILEPATH="https://build.phonegap.com/apps/"
+$APIcall="#{$APIPATH}/#{$project}"
+$creds="#{$username}:#{bp}"
+
+
 appPath="d:\\git_wa\\mekarer\\" 
 projectPath="d:\\git_wa\\mekarer\\apk" 
-
-APIPATH="https://build.phonegap.com/api/v1/apps";
-FILEPATH="https://build.phonegap.com/apps/";
 
 def get_password(prompt="Enter Password")
    ask(prompt) {|q| q.echo = false}
 end
 
-bp="c2l4cGVucw==".unpack("m")[0] #get_password("Enter PhoneGap Build password")
-
-APIcall="#{APIPATH}/#{project}"
-creds="#{username}:#{bp}";
 
 ##commit changes
 puts "Forcing changes to github";
@@ -29,35 +29,36 @@ puts "Forcing changes to github";
 `git push origin master`
 puts "Done";
 
-def rebuild(creds) 
+def rebuild() 
+
 	##Request Phonegap data
 	puts "Requesting Project Data.";
 
-	blob=`curl -s -u #{creds} #{APIcall}`
+	blob=`curl -s -u #{$creds} #{$APIcall}`
 	title = blob['title']
 	package = blob['package']
 
 	##Request Rebuild
 	puts "Requesting Rebuild."
-	request=`curl -s -u #{creds} -X PUT -d 'data={"pull":"true"}' #{APIcall}`
+	request=`curl -s -u #{$creds} -X PUT -d 'data={"pull":"true"}' #{$APIcall}`
 	puts "Done. "
 	donecheck=""
 
-	print "[/]"
+	print "[: - "
 
-	until JSON.parse(`curl -s -u #{creds}  #{APIcall}`)['status']['android'] == "complete"
+	until JSON.parse(`curl -s -u #{$creds}  #{$APIcall}`)['status']['android'] == "complete"
 		print "."
 	end	
 
-	puts
+	puts "]"
 	puts "Done. Now downloading."
 
-	download=`curl -L -s -u #{creds} -o #{title}-debug.apk #{FILEPATH}/#{project}/download/android`
+	download=`curl -L -s -u #{$creds} -o #{title}-debug.apk #{FILEPATH}/#{$project}/download/android`
 
 	`adb uninstall #{package}`
 	`adb install -r ./#{title}-debug.apk`
 end
 
 Dir.chdir("#{projectPath}") do 
-	rebuild(creds)
+	rebuild()
 end
