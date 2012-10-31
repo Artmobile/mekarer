afc602c87380fbd1593f20dfe659d10aaa62ea73
Artmobile <artmobile@gmail.com>
2012-10-31 13:52:44 +0200

    auto commit as part of script

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,7 +29,7 @@ function onDeviceReady() {
             dataSource.read();
         }
         */
-        console.log("Haleluyah! " + data + "\n");
+        console.log("Haleluyah! "  + "\n");
     }).error(function(err){ console.log("Error occurred: " + err); });
 
 
